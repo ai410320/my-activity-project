0d8ed64ae2d0358753072b2df4b7f19ef0d6564e
ai410320 <ai410320@gmail.com>
2025-03-11 16:04:03 +0800

    import path fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ff_promo_04 from './pages/ff_promo_04/index.vue'
 import ff_promo_05 from './pages/ff_promo_05/index.vue'
-import ff_promo_06 from './pages/ff_promo_05/index.vue'
-import ff_promo_07 from './pages/ff_promo_05/index.vue'
-import ff_promo_08 from './pages/ff_promo_05/index.vue'
-import ff_promo_09 from './pages/ff_promo_05/index.vue'
-import ff_promo_10 from './pages/ff_promo_05/index.vue'
+import ff_promo_06 from './pages/ff_promo_06/index.vue'
+import ff_promo_07 from './pages/ff_promo_07/index.vue'
+import ff_promo_08 from './pages/ff_promo_08/index.vue'
+import ff_promo_09 from './pages/ff_promo_09/index.vue'
+import ff_promo_10 from './pages/ff_promo_10/index.vue'
 
 export { ff_promo_04, ff_promo_05, ff_promo_06, ff_promo_07, ff_promo_08, ff_promo_09, ff_promo_10 }
