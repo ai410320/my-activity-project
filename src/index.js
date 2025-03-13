f42ff0c800351674e5838fcfeb94c6eb92d74ffb
ai410320 <ai410320@gmail.com>
2025-03-13 10:28:33 +0800

    add module name

--- src/index.js
+++ src/index.js
@@ -9,12 +9,18 @@
 // export { ff_promo_04, ff_promo_05, ff_promo_06, ff_promo_07, ff_promo_08, ff_promo_09, ff_promo_10 }
 
 const modules = import.meta.glob('./pages/ff_promo_*/index.vue', { eager: true })
+const modules050 = import.meta.glob('/pages/050/ff_promo_*/index.vue', { eager: true })
+const modulesKY = import.meta.glob('/pages/ky/ff_promo_*/index.vue', { eager: true })
 
-const components = Object.keys(modules).reduce((acc, path) => {
-    // 取得檔名，例如 `./pages/ff_promo_04/index.vue` → `ff_promo_04`
-    const name = path.match(/ff_promo_\d+/)[0]
-    acc[name] = modules[path].default
-    return acc
-}, {})
+function generateComponentMap(modules) {
+    return Object.keys(modules).reduce((acc, path) => {
+        const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
+        if (name) acc[name] = modules[path].default
+        return acc
+    }, {})
+}
 
-export default components
+const module050 = generateComponentMap(modules050)
+const moduleKY = generateComponentMap(modulesKY)
+
+export default { module050, moduleKY }
