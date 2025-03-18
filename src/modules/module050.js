65f4b987eff91f7696dc9ce2d7741a28abccd110
ai410320 <ai410320@gmail.com>
2025-03-18 17:40:36 +0800

    modify module050.js

--- src/modules/module050.js
+++ src/modules/module050.js
@@ -1,23 +1,23 @@
-const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
+// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
 // const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
-function generateComponentMap(modules) {
-    return Object.keys(modules).reduce((acc, path) => {
-        const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
-        if (name) acc[name] = modules[path].default
-        return acc
-    }, {})
-}
+// function generateComponentMap(modules) {
+//     return Object.keys(modules).reduce((acc, path) => {
+//         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
+//         if (name) acc[name] = modules[path].default
+//         return acc
+//     }, {})
+// }
 
 // const module050 = generateComponentMap(modules050)
 // export default module050
 
-export const module050 = generateComponentMap(modules050)
-// function loadComponent(name) {
-//     // 只載入符合 `ff_promo_XX` 的模組
+// export const module050 = generateComponentMap(modules050)
+function loadComponent(name) {
+    // 只載入符合 `ff_promo_XX` 的模組
 
-//     const match = Object.keys(modules050).find((path) => path.includes(name))
-//     console.log('wewewewewe', modules050, match, name)
-//     return match ? modules050[match] : null
-// }
+    const match = Object.keys(modules050).find((path) => path.includes(name))
+    console.log('wewewewewe', modules050, match, name)
+    return match ? modules050[match].default : null
+}
 
-// export { loadComponent }
+export { loadComponent }
