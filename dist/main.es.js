628656406a22ba64137e87fb31b9af297b9af974
ai410320 <ai410320@gmail.com>
2025-03-18 17:45:58 +0800

    modify module050.js

--- dist/main.es.js
+++ dist/main.es.js
@@ -1,8 +1,8 @@
-import { loadComponent as p } from "./module050.es.js";
+import { loadComponent as e } from "./module050.es.js";
 import { moduleH5poker as d } from "./moduleH5poker.es.js";
 import "vue";
 import "./style.8ae2acf7.js";
 export {
-  p as module050,
+  e as loadComponent,
   d as moduleH5poker
 };
