1161316a7d4738d2509f2c256b0c759ba128ca44
ai410320 <ai410320@gmail.com>
2025-03-18 14:13:28 +0800

    modify module050.js

--- src/module050.js
+++ src/module050.js
@@ -1,11 +1,20 @@
-const modules050 = import.meta.glob('./pages/050/ff_promo_*/index.vue', { eager: true })
+// const modules050 = import.meta.glob('./pages/050/ff_promo_*/index.vue', { eager: true })
+const modules050 = import.meta.glob('./pages/050/ff_promo_*/index.vue')
 
-function generateComponentMap(modules) {
-    return Object.keys(modules).reduce((acc, path) => {
-        const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
-        if (name) acc[name] = modules[path].default
-        return acc
-    }, {})
+// function generateComponentMap(modules) {
+//     return Object.keys(modules).reduce((acc, path) => {
+//         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
+//         if (name) acc[name] = modules[path].default
+//         return acc
+//     }, {})
+// }
+
+function loadComponent(name) {
+    // 只載入符合 `ff_promo_XX` 的模組
+    const match = Object.keys(modules050).find((path) => path.includes(name))
+    return match ? modules050[match]() : null
 }
 
-export const module050 = generateComponentMap(modules050)
+export { loadComponent }
+
+// export const module050 = generateComponentMap(modules050)
