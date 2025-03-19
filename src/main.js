5dbcaae480190be47382cdf1d26f6cdb977a754e
ai410320 <ai410320@gmail.com>
2025-03-19 11:40:58 +0800

    modify module050.js

--- src/main.js
+++ src/main.js
@@ -18,6 +18,26 @@ const router = createRouter({
 })
 console.log(generatedRoutes)
 
+// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
+// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
+// function generateComponentMap(modules) {
+//     return Object.keys(modules).reduce((acc, path) => {
+//         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
+//         if (name) acc[name] = modules[path].default
+//         return acc
+//     }, {})
+// }
+// function loadComponent(name) {
+//     // 只載入符合 `ff_promo_XX` 的模組
+
+//     const match = Object.keys(modules050).find((path) => path.includes(name))
+//     console.log('wewewewewe', modules050, match, name, modules050[match].default)
+//     return match ? modules050[match].default : null
+// }
+
+// console.log('wewewe', generateComponentMap(modules050))
+// console.log('wewewe', loadComponent('ff_promo_04'))
+
 // app.mount("#app");
 app.use(router)
 router.isReady().then(() => {
