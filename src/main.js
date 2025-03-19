4464da2721474a78dbae911977b046f13a128aba
ai410320 <ai410320@gmail.com>
2025-03-19 11:44:52 +0800

    modify module050.js

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ const router = createRouter({
 console.log(generatedRoutes)
 
 // const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
-// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
+// // const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
 // function generateComponentMap(modules) {
 //     return Object.keys(modules).reduce((acc, path) => {
 //         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
