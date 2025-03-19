1f944ae8bc724c369a8a70d78451a70ea9b5c98e
ai410320 <ai410320@gmail.com>
2025-03-19 12:21:26 +0800

    modify module050.js

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ const router = createRouter({
 console.log(generatedRoutes)
 
 // const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
-// // const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
+// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
 // function generateComponentMap(modules) {
 //     return Object.keys(modules).reduce((acc, path) => {
 //         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
@@ -27,11 +27,14 @@ console.log(generatedRoutes)
 //         return acc
 //     }, {})
 // }
-// function loadComponent(name) {
+
+// async function loadComponent(name) {
 //     // 只載入符合 `ff_promo_XX` 的模組
 
 //     const match = Object.keys(modules050).find((path) => path.includes(name))
-//     console.log('wewewewewe', modules050, match, name, modules050[match].default)
+//     console.log('wewewewewe', modules050, match, name, modules050[match]())
+//     const module = await modules050[match]()
+//     console.log('wewewewewe', module.default)
 //     return match ? modules050[match].default : null
 // }
 
--- src/modules/module050.js
+++ src/modules/module050.js
@@ -1,5 +1,5 @@
-const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
-// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
+// const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager: true })
+const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue')
 // function generateComponentMap(modules) {
 //     return Object.keys(modules).reduce((acc, path) => {
 //         const name = path.match(/ff_promo_\d+/)?.[0] // 取得 `ff_promo_XX`
@@ -12,12 +12,13 @@ const modules050 = import.meta.glob('@/pages/050/ff_promo_*/index.vue', { eager:
 // export default module050
 
 // export const module050 = generateComponentMap(modules050)
-function loadComponent(name) {
+async function loadComponent(name) {
     // 只載入符合 `ff_promo_XX` 的模組
 
     const match = Object.keys(modules050).find((path) => path.includes(name))
     console.log('wewewewewe', modules050, match, name, modules050[match], modules050[match].default)
-    return match ? modules050[match].default : null
+    const module = await modules050[match]()
+    return match ? module.default : null
 }
 
 export { loadComponent }
